4576d3f3643f3ea665c8f3399b6ea0fe7801947e
Maddy Leger <maddyleger1@gmail.com>
2020-11-21 16:21:17 -0500

    update nugets

--- PlantLady/PlantLady/Views/PlantsPage.xaml.cs
+++ PlantLady/PlantLady/Views/PlantsPage.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,5 +15,6 @@ namespace PlantLady.Views
         {
             InitializeComponent();
         }
+
     }
 }
