cf176c115cdd0eb6ac77456b27bbed4c93f6c053
Sweekriti Satpathy <swsat@microsoft.com>
2022-04-01 15:14:12 -0700

    android builds no errors

--- PlantLady/PlantLady.Android/MainActivity.cs
+++ PlantLady/PlantLady.Android/MainActivity.cs
@@ -14,20 +14,20 @@ namespace PlantLady.Droid
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+            //TabLayoutResource = Resource.Layout.Tabbar;
+            //ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
 
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
+            //Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            //global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            //LoadApplication(new App());
         }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        //public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        //{
+        //    Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
+        //    base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        //}
     }
 }
--- PlantLady/PlantLady/AssemblyInfo.cs
+++ PlantLady/PlantLady/AssemblyInfo.cs
@@ -1,7 +1,3 @@
 using Microsoft.Maui;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-
-[assembly: ExportFont("Michella-Garden.otf", Alias = "Michella")]
-[assembly: ExportFont("AmaticSC-Regular.ttf", Alias = "Amatic")]
-[assembly: ExportFont("materialdesignicons-webfont.ttf", Alias = "MaterialIcons")]
