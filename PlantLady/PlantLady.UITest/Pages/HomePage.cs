30fa1bff4393808e9b5b59fca497b63f0b9b66a9
Sweekriti Satpathy <swsat@microsoft.com>
2023-05-01 19:48:41 -0700

    adds Xamarin UITESTs for migration

---
+++ PlantLady/PlantLady.UITest/Pages/HomePage.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using NUnit.Framework;
+using Query = System.Func<Xamarin.UITest.Queries.AppQuery, Xamarin.UITest.Queries.AppQuery>;
+
+namespace PlantLady.UITest.Pages
+{
+    public class HomePage : BasePage
+    {
+        readonly Query cardItem;
+        readonly Query MyPlantsTab;
+        readonly Query CareTab;
+        readonly Query MyPlantsPage;
+        readonly Query plantName;
+
+        protected override PlatformQuery Trait => new PlatformQuery
+        {
+            Android = x => x.Marked("Plant Care"),
+            iOS = x => x.Marked("Plant Care")
+        };
+
+        public HomePage()
+        {
+            cardItem = x => x.Marked("Water");
+            MyPlantsTab = x => x.Marked("My Plants");
+            CareTab = x => x.Marked("Care");
+            MyPlantsPage = x => x.Marked("Todo: Add swipe views!");
+            plantName = x => x.Marked("Dracaena");
+        }
+
+        public void ChangeTabsTest()
+        {
+            app.WaitForElement(MyPlantsTab);
+            app.Tap(MyPlantsTab);
+            app.WaitForElement(MyPlantsPage);
+            app.Screenshot("On My Plants Tab");
+
+            app.Tap(CareTab);
+            app.WaitForElement(Trait.Current);
+            app.Screenshot("On Care Tab");
+        }
+
+        public void SwipeToPlant()
+        {
+            app.SwipeRightToLeft(cardItem);
+            app.SwipeRightToLeft(cardItem);
+            app.WaitForElement(plantName, timeout: TimeSpan.FromSeconds(5));
+            app.Screenshot("On new Plant");
+        }
+        
+    }
+}
---
+++ PlantLady/PlantLady.UITest/Tests/Tests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using PlantLady.UITest.Pages;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace PlantLady.UITest
+{
+    public class Tests : BaseTestFixture
+    {
+        public Tests(Platform platform)
+        : base(platform)
+        {
+        }
+
+        [Test]
+        public void Repl()
+        {
+            app.Repl();
+        }
+
+        [Test]
+        public void ChangeTabsTest()
+        {
+            new HomePage()
+                .ChangeTabsTest();
+
+        }
+
+        [Test]
+        public void SwipePlantTest()
+        {
+            new HomePage()
+                .SwipeToPlant();
+        }
+    }
+}
